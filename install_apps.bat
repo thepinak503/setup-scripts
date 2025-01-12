c6e473a81f2c84e99291d161a051be83d825ac4e
Pinak Dhabu <thepinak503@duck.com>
2025-01-12 09:44:53 +0530

    Update install_apps.bat

--- install_apps.bat
+++ install_apps.bat
@@ -1,117 +1,162 @@
 @echo off
+
 :: Company: Alex313031
+echo Installing "Mercury"...
 winget install Alex313031.Mercury --accept-package-agreements
+echo Installing "Thorium.AVX2"...
 winget install Alex313031.Thorium.AVX2 --accept-package-agreements
 
 :: Company: BCUninstaller
+echo Installing "BCUninstaller"...
 winget install BCUninstaller --accept-package-agreements
 
 :: Company: Bleachbit
+echo Installing "Bleachbit"...
 winget install Bleachbit --accept-package-agreements
 
 :: Company: CrystalDewWorld
+echo Installing "CrystalDiskInfo"...
 winget install CrystalDewWorld.CrystalDiskInfo --accept-package-agreements
+echo Installing "CrystalDiskMark"...
 winget install CrystalDewWorld.CrystalDiskMark --accept-package-agreements
 
 :: Company: CPUID
+echo Installing "CPU-Z"...
 winget install CPUID.CPU-Z --accept-package-agreements
+echo Installing "HWMonitor"...
 winget install CPUID.HWMonitor --accept-package-agreements
 
 :: Company: dupeguru
+echo Installing "DupeGuru"...
 winget install dupeguru.DupeGuru --accept-package-agreements
 
 :: Company: Fastfetch
+echo Installing "Fastfetch"...
 winget install Fastfetch --accept-package-agreements
 
 :: Company: FlowLauncher
+echo Installing "FlowLauncher"...
 winget install FlowLauncher.FlowLauncher --accept-package-agreements
 
 :: Company: Google
+echo Installing "Google Drive"...
 winget install Google.GoogleDrive --accept-package-agreements
+echo Installing "Chrome"...
 winget install google.chrome --accept-package-agreements
 
 :: Company: HWiNFO
+echo Installing "HWiNFO"...
 winget install HWiNFO --accept-package-agreements
 
 :: Company: mulaRahul
+echo Installing "Keyviz"...
 winget install mulaRahul.Keyviz --accept-package-agreements
 
 :: Company: Microsoft
+echo Installing "PowerShell"...
 winget install Microsoft.PowerShell --accept-package-agreements
+echo Installing "PowerToys"...
 winget install Microsoft.PowerToys --accept-package-agreements
+echo Installing "Visual Studio Code"...
 winget install Microsoft.VisualStudioCode --accept-package-agreements
+echo Installing "Windows Terminal"...
 winget install Microsoft.windowsterminal --accept-package-agreements
 
 :: Company: Motrix (from Microsoft Store)
+echo Installing "Motrix"...
 winget install motrix -s msstore --accept-package-agreements
 
 :: Company: Notepad++
+echo Installing "Notepad++"...
 winget install notepad++ --accept-package-agreements
 
 :: Company: Notepads app (from Microsoft Store)
+echo Installing "Notepads app"...
 winget install "notepads app" --accept-package-agreements
 
 :: Company: OBSProject
+echo Installing "OBS Studio"...
 winget install OBSProject.OBSStudio --accept-package-agreements
 
 :: Company: qBittorrent
+echo Installing "qBittorrent"...
 winget install qBittorrent.qBittorrent --accept-package-agreements
 
 :: Company: ShareX
+echo Installing "ShareX"...
 winget install sharex.sharex --accept-package-agreements
 
 :: Company: sharkdp
+echo Installing "bat"...
 winget install sharkdp.bat --accept-package-agreements
 
 :: Company: SumatraPDF
+echo Installing "SumatraPDF"...
 winget install SumatraPDF --accept-package-agreements
 
 :: Company: SuperF4
+echo Installing "SuperF4"...
 winget install SuperF4 --accept-package-agreements
 
 :: Company: TwinkleTray
+echo Installing "TwinkleTray"...
 winget install TwinkleTray --accept-package-agreements
 
 :: Company: VideoLAN
+echo Installing "VLC"...
 winget install VideoLAN.VLC --accept-package-agreements
 
 :: Company: Winaero
+echo Installing "Winaero Tweaker"...
 winget install Winaero.Tweaker --accept-package-agreements
 
 :: Company: WinDirStat
+echo Installing "WinDirStat"...
 winget install WinDirStat --accept-package-agreements
 
 :: Company: WizTree
+echo Installing "WizTree"...
 winget install WizTree --accept-package-agreements
 
 :: Company: xdm (from Microsoft Store)
+echo Installing "XDM"...
 winget install xdm -s msstore --accept-package-agreements
 
 :: Company: Unigram (from Microsoft Store)
+echo Installing "Unigram"...
 winget install Unigram -s msstore --accept-package-agreements
 
 :: Company: Mozilla
+echo Installing "Firefox"...
 winget install Mozilla.Firefox --accept-package-agreements
 
 :: Company: Everything
+echo Installing "Everything"...
 winget install voidtools.everything --accept-package-agreements
 
 :: Company: Nilesoft
+echo Installing "Shell"...
 winget install Nilesoft.Shell --accept-package-agreements
 
 :: Company: 7-Zip
+echo Installing "7-Zip"...
 winget install 7-Zip.7zip --accept-package-agreements
 
 :: Company: StartAllBack (Uncommented if needed)
+echo Installing "StartAllBack"...
 winget install StartAllBack --accept-package-agreements
 
 :: Company: Whatsapp (from Microsoft Store)
+echo Installing "Whatsapp"...
 winget install Whatsapp -s msstore --accept-package-agreements
 
 :: Company: ChatGPT (from Microsoft Store)
+echo Installing "ChatGPT"...
 winget install chatgpt -s msstore --accept-package-agreements
 
 :: Company : Zyedidia
+echo Installing "micro"...
 winget install zyedidia.micro --accept-package-agreements
+
 pause
 exit
