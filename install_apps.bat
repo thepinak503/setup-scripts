5d3c287951ce4e33cb5e1fe82086289d154ab38e
Pinak Dhabu <thepinak503@duck.com>
2025-01-13 16:00:09 +0530

    Update install_apps.bat

--- install_apps.bat
+++ install_apps.bat
@@ -1,1 +1,113 @@
+@echo off
+:: Company: Alex313031
+winget install Alex313031.Mercury --accept-package-agreements
+winget install Alex313031.Thorium.AVX2 --accept-package-agreements
 
+:: Company: BCUninstaller
+winget install BCUninstaller --accept-package-agreements
+
+:: Company: Bleachbit
+winget install Bleachbit --accept-package-agreements
+
+:: Company: CrystalDewWorld
+winget install CrystalDewWorld.CrystalDiskInfo --accept-package-agreements
+winget install CrystalDewWorld.CrystalDiskMark --accept-package-agreements
+
+:: Company: CPUID
+winget install CPUID.CPU-Z --accept-package-agreements
+winget install CPUID.HWMonitor --accept-package-agreements
+
+:: Company: dupeguru
+winget install dupeguru.DupeGuru --accept-package-agreements
+
+:: Company: Fastfetch
+winget install Fastfetch --accept-package-agreements
+
+:: Company: FlowLauncher
+winget install FlowLauncher.FlowLauncher --accept-package-agreements
+
+:: Company: Google
+winget install Google.GoogleDrive --accept-package-agreements
+winget install google.chrome --accept-package-agreements
+
+:: Company: HWiNFO
+winget install HWiNFO --accept-package-agreements
+
+:: Company: mulaRahul
+winget install mulaRahul.Keyviz --accept-package-agreements
+
+:: Company: Microsoft
+winget install Microsoft.PowerShell --accept-package-agreements
+winget install Microsoft.PowerToys --accept-package-agreements
+winget install Microsoft.VisualStudioCode --accept-package-agreements
+winget install Microsoft.windowsterminal --accept-package-agreements
+
+:: Company: Motrix (from Microsoft Store)
+winget install motrix -s msstore --accept-package-agreements
+
+:: Company: Notepad++
+winget install notepad++ --accept-package-agreements
+
+:: Company: Notepads app (from Microsoft Store)
+winget install "notepads app" --accept-package-agreements
+
+:: Company: OBSProject
+winget install OBSProject.OBSStudio --accept-package-agreements
+
+:: Company: qBittorrent
+winget install qBittorrent.qBittorrent --accept-package-agreements
+
+:: Company: ShareX
+winget install sharex.sharex --accept-package-agreements
+
+:: Company: sharkdp
+winget install sharkdp.bat --accept-package-agreements
+
+:: Company: SumatraPDF
+winget install SumatraPDF --accept-package-agreements
+
+:: Company: SuperF4
+winget install SuperF4 --accept-package-agreements
+
+:: Company: TwinkleTray
+winget install TwinkleTray --accept-package-agreements
+
+:: Company: VideoLAN
+winget install VideoLAN.VLC --accept-package-agreements
+
+:: Company: Winaero
+winget install Winaero.Tweaker --accept-package-agreements
+
+:: Company: WinDirStat
+winget install WinDirStat --accept-package-agreements
+
+:: Company: WizTree
+winget install WizTree --accept-package-agreements
+
+:: Company: xdm (from Microsoft Store)
+winget install xdm -s msstore --accept-package-agreements
+
+:: Company: Unigram (from Microsoft Store)
+winget install Unigram -s msstore --accept-package-agreements
+
+:: Company: Mozilla
+winget install Mozilla.Firefox --accept-package-agreements
+
+:: Company: Everything
+winget install Everything.Search --accept-package-agreements
+
+:: Company: Nilesoft
+winget install Nilesoft.Shell --accept-package-agreements
+
+:: Company: 7-Zip
+winget install 7-Zip.7zip --accept-package-agreements
+
+:: Company: StartAllBack (Uncommented if needed)
+winget install StartAllBack --accept-package-agreements
+
+:: Company: Whatsapp (from Microsoft Store)
+winget install Whatsapp -s msstore --accept-package-agreements
+
+:: Company: ChatGPT (from Microsoft Store)
+winget install chatgpt -s msstore --accept-package-agreements
+pause
