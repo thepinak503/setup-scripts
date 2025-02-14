9daf2c27d3ef0f41f5a4dc13afe2e8f9c5c60afc
Pinak Dhabu <thepinak503@duck.com>
2025-02-14 08:55:03 +0500

    Create new.cmd

---
+++ new.cmd
@@ -0,0 +1,4 @@
+pwsh Remove-Item "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
+pwsh Remove-Item "$env:LOCALAPPDATA\Microsoft\PowerShell\PowerShell\history.txt" 
+powershell Remove-Item "$env:LOCALAPPDATA\Microsoft\PowerShell\PowerShell\history.txt"
+powershell Remove-Item "$env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
